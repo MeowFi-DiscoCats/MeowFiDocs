d0f12fcc5773cd5159c7be8c4468dce2c63fb397
Haroon Saifi <haroonsaifi2001@gmail.com>
2025-03-15 15:22:16 +0530

    initial commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,28 +1,38 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-import starlight from '@astrojs/starlight';
+import { defineConfig } from "astro/config";
+import starlight from "@astrojs/starlight";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [
-		starlight({
-			title: 'My Docs',
-			social: {
-				github: 'https://github.com/withastro/starlight',
-			},
-			sidebar: [
-				{
-					label: 'Guides',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', slug: 'guides/example' },
-					],
-				},
-				{
-					label: 'Reference',
-					autogenerate: { directory: 'reference' },
-				},
-			],
-		}),
-	],
+  vite: {
+    plugins: [tailwindcss()],
+  },
+  integrations: [
+    starlight({
+      title: "DiscoCats",
+      social: {
+        discord: "https://discord.com/invite/RZWntTWFrb",
+      },
+      logo: {
+        src: "./src/assets/logo.avif",
+        alt: "DiscoCats Logo",
+      },
+      components: {
+        PageTitle: "./src/components/title.astro",
+      },
+      customCss: ["./src/styles/theme.css"],
+      sidebar: [
+        { label: "Introduction to MeowFi", slug: "" },
+        {
+          label: "Guides",
+          items: [{ label: "Example Guide", slug: "guides/example" }],
+        },
+        {
+          label: "Reference",
+          autogenerate: { directory: "reference" },
+        },
+      ],
+    }),
+  ],
 });
